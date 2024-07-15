696bd7fde5f442850f9ff2f307eb21e86dc7368d
Boran Ozkan <boranozkan11@gmail.com>
2024-07-15 14:16:50 +0300

    Added KitchenObjSO

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjSO kitchenObjSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         print("Interacted!");
+        Transform kitchenObjTransform = Instantiate(kitchenObjSO.prefab, counterTopPoint);
+        kitchenObjTransform.localPosition = Vector3.zero;
     }
 }
