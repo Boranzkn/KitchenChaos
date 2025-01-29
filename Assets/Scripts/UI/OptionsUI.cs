f47840fc49c7b2965bd4eaf1cb9e27d7fbdfc239
Boran Ozkan <boranozkan11@gmail.com>
2025-01-29 21:31:38 +0300

    Synced Pause Logic

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -113,7 +113,7 @@ public class OptionsUI : MonoBehaviour
 
     private void Start()
     {
-        GameManager.Instance.OnGameUnpaused += GameManager_OnGameUnpaused;
+        GameManager.Instance.OnLocalGameUnpaused += GameManager_OnGameUnpaused;
 
         UpdateVisual();
         Hide();
